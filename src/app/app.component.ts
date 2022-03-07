0e34dbc1cef339ce207ad0e1f83e810fc1969220
bbr <bbr@audaxis.com>
2022-03-07 16:44:27 +0100

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Contact } from './contact';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'contact01';
+  contacts: Contact[] = [
+    {
+      id: 1,
+      prenom: 'Benjamin',
+      nom: 'Brunquers',
+      email: 'bbr@audaxis.com',
+      dateNaissance: '19/07/1991',
+    },
+  ];
+  isTableView = true;
+
+  addNewContact(newContact: Contact) {
+    this.contacts.push(newContact);
+    this.isTableView = true;
+  }
 }
